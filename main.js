1d4c9c5498c9ea78aec990b726454303282be691
kandd871 <114494325+kandd871@users.noreply.github.com>
2024-06-01 00:08:58 +0530

    first-upload

---
+++ main.js
@@ -0,0 +1,58 @@
+window.onload = function() {
+            // Existing JavaScript code
+            const carousel = document.getElementById('one');
+            const imageCount = 17;
+
+            for (let i = 1; i <= imageCount; i++) {
+                const img = document.createElement('img');
+                img.src = `imgs/homebook/${i}.png`;
+                carousel.appendChild(img);
+            }
+
+            const carousel2 = document.getElementById('two');
+            const imageCount2 = 12;
+
+            for (let i = 1; i <= imageCount2; i++) {
+                const img = document.createElement('img');
+                img.src = `imgs/chamberlain/${i}.png`;
+                carousel2.appendChild(img);
+            }
+
+            const carousel3 = document.getElementById('three');
+            const imageCount3 = 16;
+
+            for (let i = 1; i <= imageCount3; i++) {
+                const img = document.createElement('img');
+                img.src = `imgs/stardew/${i}.png`;
+                carousel3.appendChild(img);
+            }
+
+            const carousel4 = document.getElementById('four');
+            const imageCount4 = 10;
+
+            for (let i = 1; i <= imageCount4; i++) {
+                const img = document.createElement('img');
+                img.src = `imgs/blumenhaus/${i}.png`;
+                carousel4.appendChild(img);
+            }
+
+            const carousel5 = document.getElementById('five');
+            const imageCount5 = 10;
+
+            for (let i = 2; i <= imageCount5; i++) {
+                const img = document.createElement('img');
+                img.src = `imgs/weaving/${i}.png`;
+                carousel5.appendChild(img);
+            }
+
+            const carousel6 = document.getElementById('six');
+            const imageCount6 = 15;
+
+            for (let i = 2; i <= imageCount6; i++) {
+                const img = document.createElement('img');
+                img.src = `imgs/escapism1/${i}.png`;
+                carousel6.appendChild(img);
+            }
+
+            
+        };
